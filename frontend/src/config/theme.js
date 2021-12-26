45cdbaa6222389a20958a0b1a53e05127fc3296b
Daniel Kamnitzer <dany.kamnitzer@hotmail.de>
2021-12-26 16:30:53 +0100

    MUI Theme

---
+++ frontend/src/config/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+	typography: {
+		fontFamily: [
+			"Montserrat",
+		]
+	},
+	palette: {}
+})
+
+export default theme
