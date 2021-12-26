9be4996dc3f6ac31788a5762b509e4d096598dc1
Daniel Kamnitzer <dany.kamnitzer@hotmail.de>
2021-12-26 16:28:20 +0100

    Routing

---
+++ frontend/src/routes/home.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { Bar } from "../components"
+import * as API from "../api"
+
+export default function App() {
+    const [recommendations, setRecommendations] = useState(null)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const response = await API.recommendations.team(1)
+            setRecommendations(await response.json())
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    return (
+            <Bar />
+    )
+}
