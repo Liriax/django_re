c23fbd0fa4e27d8747b49d3f244ec339cf17c989
niceric3 <richard@Richards-MacBook-Air.local>
2021-12-29 01:54:53 +0100

    add close button for recommendationdetails

--- frontend/src/components/RecommendationDetails.js
+++ frontend/src/components/RecommendationDetails.js
@@ -1,6 +1,9 @@
 export default function RecommendationDetails(props) {
-    const { recommendation } = props
-    return (
-        <div>{JSON.stringify(recommendation)}</div>
-    )
+  const { recommendation, onClose } = props;
+  return (
+    <div>
+      <button onClick={onClose}>close</button>
+      <div>{JSON.stringify(recommendation)}</div>
+    </div>
+  );
 }
