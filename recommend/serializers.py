4ed754e5629f298e59a52b234551581e5bed43ec
niceric3 <93085129+niceric3@users.noreply.github.com>
2022-01-03 03:15:15 +0100

    Add Functionality required for statistics display to api (#7)
    * start adding measurementgetter to api
    
    * add measurement_id to dora api call
    
    * add url for api
    
    * replace id with object_id
    
    * work on api
    
    Co-authored-by: niceric3 <richard@Richards-MacBook-Air.local>
    Co-authored-by: niceric3 <richard@Richards-Air.fritz.box>

--- recommend/serializers.py
+++ recommend/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Recommendation, SuggestedRecommendation
+from .models import SuggestedRecommendation, Measurement
 
 class SuggestedRecommendationSerializer(serializers.ModelSerializer):
     recommendation_headline = serializers.CharField(source='recommendation.headline')
@@ -19,3 +19,26 @@ class SuggestedRecommendationSerializer(serializers.ModelSerializer):
             'team_name'
         )
         model = SuggestedRecommendation
+
+class MeasurementObjectRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        if isinstance(value, Metric):
+            return f'Metric: {value.name}'
+        elif isinstance(value, Dora_kpi):
+            return f'DORA KPI: {value.name}'
+        raise Exception('Unexpected type of tagged object')
+        
+class MeasurementSerializer(serializers.ModelSerializer):
+    measured_metric = MeasurementObjectRelatedField(read_only=True)
+    team_name = serializers.CharField(source='team.name')
+    class Meta:
+        fields=(
+            'id',
+            'measured_at',
+            'measured_metric',
+            'object_id',
+            'value',
+            'team',
+            'team_name'
+        )
+        model=Measurement
